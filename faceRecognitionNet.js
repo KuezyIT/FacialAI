40beedb9efe8db6b721f2c8149d1d20ec25d0e60
Sven <kuezy@protonmail.com>
2023-01-03 23:00:34 +0100

    Push 0011 - edited facialRecognitionNet.js

--- faceRecognitionNet.js
+++ faceRecognitionNet.js
@@ -1,10 +1,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/FacialAi/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/FacialAi/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/FacialAi/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/FacialAi/models')
 ]).then(startVideo)
 
 function startVideo() {
