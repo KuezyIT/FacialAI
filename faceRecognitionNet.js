f8f7065f4d9b64d271276416483325a3da16bf9d
Sven <kuezy@protonmail.com>
2023-01-04 10:35:18 +0100

    Push 0017 - Added ageAndGender calculation

--- faceRecognitionNet.js
+++ faceRecognitionNet.js
@@ -5,6 +5,7 @@ Promise.all([
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
   faceapi.nets.faceExpressionNet.loadFromUri("/models"),
+  faceapi.nets.ageGenderNet.loadFromUri('/models')
 ]).then(startVideo);
 
 function startVideo() {
@@ -15,7 +16,7 @@ function startVideo() {
   );
 }
 
-video.addEventListener("playing", () => {
+video.addEventListener("play", () => {
   const canvas = faceapi.createCanvasFromMedia(video);
   document.body.append(canvas);
   const displaySize = { width: video.width, height: video.height };
@@ -24,11 +25,22 @@ video.addEventListener("playing", () => {
     const detections = await faceapi
       .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
       .withFaceLandmarks()
-      .withFaceExpressions();
+      .withFaceExpressions()
+      .withAgeAndGender();
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     faceapi.draw.drawDetections(canvas, resizedDetections);
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    resizedDetections.forEach(result => {
+      const { age, gender, genderProbability } = result;
+      new faceapi.draw.DrawTextField(
+        [
+          `${faceapi.round(age, 0)} years`,
+          `${gender} (${faceapi.round(genderProbability)})`
+        ],
+        result.detection.box.bottomRight
+      ).draw(canvas);
+    });
   }, 100);
 });
